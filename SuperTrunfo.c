2d1045187f29eb6bc6d32d641481078cb3e0e280
canaldomrcelo <canaldomrcelo@gmail.com>
2025-02-05 01:57:46 -0300

    Update SuperTrunfo.c

--- SuperTrunfo.c
+++ SuperTrunfo.c
@@ -3,9 +3,9 @@
 (char) Acre 
 (char[]) A01
 (char[]) Capixaba 
-(int) População
-(float) Área em km²
-(float) PIB 
-(int) Número de pontos turísticos
+(int) População: 3,834 milhões
+(float) Área em km²: 46.095 km² 
+(float) PIB: 238,3 bilhões
+(int) Número de pontos turísticos: 15
 
 return
