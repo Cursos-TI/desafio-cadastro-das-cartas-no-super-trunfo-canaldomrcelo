ed147d9e98ffe0fd17540682e704c7a59ba6ae57
canaldomrcelo <canaldomrcelo@gmail.com>
2025-02-17 21:11:59 +0000

    criei o cadastramento das Cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,7 @@ int main() {
     char estado1;
     char codigo1[10];
     char cidade1[20];
-    int população1;
+    int populacao1;
     float km1;
     float PIB1;
     int pontosturisticos1;
@@ -17,7 +17,7 @@ int main() {
     char estado2;
     char codigo2[10];
     char cidade2[20];
-    int população2;
+    int populacao2;
     float km2;
     float PIB2;
     int pontosturisticos2;
