c841e8846d9b5eb6e9db67d56c26b9d3697cca64
canaldomrcelo <canaldomrcelo@gmail.com>
2025-03-03 19:23:59 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,30 +3,11 @@
 int main() {
     
     //Declaração
-    char estado, codigo[3], cidade[20];
-    int populacao;                            //variaveis com as caracteristicas das cartas
-    float km, PIB;
-    int pontosturisticos;
+    char estado1, estado2, codigo1[3],codigo2[3], cidade1[20], cidade2[20];
+    int populacao1, populacao2, pontosturistico1, pontosturisticos2;                         //variaveis com as caracteristicas das cartas
+    float km1, km2, densidade1, densidade2, pibpercapta1, pibpercapa2;
 
-    // Carta 1
 
-    estado = 1;
-    codigo[3] = 1;
-    cidade[20] = 1;
-    populacao = 1;
-    km = 1;
-    PIB = 1;
-    pontosturisticos = 1;
-    
-    // Carta 2
-    
-    estado = 2;
-    codigo[3] = 2;
-    cidade[20] = 2;
-    populacao = 2;
-    km = 2;
-    PIB = 2;
-    pontosturisticos = 2;
 
     return 0;
 }
