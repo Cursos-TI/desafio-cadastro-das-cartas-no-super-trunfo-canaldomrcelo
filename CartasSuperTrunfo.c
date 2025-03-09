f00b35568762009dd81233d36ac1a03cdbb924fa
canaldomrcelo <canaldomrcelo@gmail.com>
2025-03-09 19:17:24 -0300

    Update CartasSuperTrunfo.c
    aplicando a logica

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,13 +1,70 @@
 #include <stdio.h> 
-
 int main() {
-    
     //Declaração
-    char estado1, estado2, codigo1[3],codigo2[3], cidade1[20], cidade2[20];
-    int populacao1, populacao2, pontosturistico1, pontosturisticos2;                         //variaveis com as caracteristicas das cartas
-    float km1, km2, densidade1, densidade2, pibpercapta1, pibpercapa2;
+    char estado;
+    char estado2;
+    char cidade1[20];
+    char cidade2[20];
+    char codigo1[3]; 
+    char codigo2[3];
+    int populacao1, populacao2;
+    int pontosturistico1, pontosturistico2;                         //variaveis com as caracteristicas das cartas
+    float pib1, pib2, area1, area2; 
+    float densidade1, densidade2, pibpercapta1, pibpercapta2;
+
+    printf("INSIRA OS DADOS DA 1º CARTA:\n");
+    
+    printf("estado de A à H: ");
+    scanf("%c", &estado);
+    
+    printf("codigo da carta: ");
+    scanf("%s", codigo1);
+    
+    printf("digite a cidade: ");
+    scanf("%s", cidade1);
+
+    printf("digite a população: ");
+    scanf("%d", &populacao1);
+
+    printf("digite a área: ");
+    scanf("%f", &area1);
+
+    printf("PIB: ");
+    scanf("%f", &pib1);
+    
+    printf("pontos turisticos: ");
+    scanf("%d", &pontosturistico1);
+    printf("carta 1 inserida com sucesso\n");
+
+    printf("Agora vamos inserir a 2º CARTA:\n");
+
+    
+    
+    printf("estado de A à H: ");
+    scanf(" %c", &estado2);
+
+    printf("codigo da carta: ");
+    scanf("%s", codigo2);
+
+    printf("digite a cidade: ");
+    scanf("%s", cidade2);
+
+    printf("digite a população: ");
+    scanf("%d", &populacao2);
+
+    printf("digite a área: ");
+    scanf("%f", &area2);
+
+    printf("PIB: ");
+    scanf("%f", &pib2);
+    
+    printf("pontos turisticos: ");
+    scanf("%d", &pontosturistico2);
+
+    printf("cartas cadastradas com sucesso");
+    
+
+    
 
 
-
-    return 0;
 }
