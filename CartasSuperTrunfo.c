6aaa1af5caeaafa3f5582bd3e6bf542d582ec45d
canaldomrcelo <canaldomrcelo@gmail.com>
2025-03-14 04:12:17 -0300

    Update CartasSuperTrunfo.c
    CONCLUSAO NIVEL AVENTUREIRO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,20 +1,12 @@
 #include <stdio.h> 
 int main() {
     //Declaração
-    char estado1;
-    char estado2;
-    char cidade1[20];
-    char cidade2[20];
-    char codigo1[5]; 
-    char codigo2[5];
-    int populacao1, populacao2;
-    int pontosturistico1, pontosturistico2;                         //variaveis com as caracteristicas das cartas
-    float pib1, pib2, area1, area2; 
-    float densidade1, densidade2, pibpercapta1, pibpercapta2;
-
+    char estado1, estado2, cidade1[20], cidade2[20], codigo1[5], codigo2[5];
+    int populacao1, populacao2, pontosturistico1, pontosturistico2;                         //variaveis com as caracteristicas das cartas
+    float pib1, pib2, area1, area2, densidade1, densidade2, pibpercapta1, pibpercapta2;
 
     printf("Bem-vindo ao Super Trunfo Cidades!\n");
-    printf("Desafie seus amigos e descubra qual cidade tem os melhores atributos.\n\n");
+    printf("Desafie seus amigos e se divirta!!!.\n\n");
     printf("INSIRA OS DADOS DA 1º CARTA:\n");
     
     printf("estado de A à H: ");
@@ -29,7 +21,7 @@ int main() {
     printf("digite a população: ");
     scanf("%d", &populacao1);
 
-    printf("digite a área: ");
+    printf("digite a área km²: ");
     scanf("%f", &area1);
 
     printf("PIB: ");
@@ -37,6 +29,10 @@ int main() {
     
     printf("pontos turisticos: ");
     scanf("%d", &pontosturistico1);
+    printf("densidade populacional: %f\n", populacao1/area1);
+    printf("PIB Percapta: %f\n", pib1/populacao1);
+
+    
     printf("----------------------------------------------\n");
     printf("CARTA 1 INSERIDA COM SUCESSO!!\n");
     printf("----------------------------------------------\n");
@@ -63,43 +59,50 @@ int main() {
     
     printf("pontos turisticos: ");
     scanf("%d", &pontosturistico2);
-
+    printf("densidade populacional: %f\n", populacao2/area2);
+    printf("PIB Percapta: %f\n", pib2/populacao2);
     printf("----------------------------------------------\n");
     printf("CARTA 2 INSERIDA COM SUCESSO!!\n");
     printf("----------------------------------------------\n");
-    printf("CARTA 01\n\n");
+    printf("CARTA 01:\n");
     
     printf("Estado: %c \n", estado1);
 
-    printf("Codigo da Carta: %s \n", codigo1);
+    printf("Codigo: %s \n", codigo1);
 
-    printf("Nome da cidade: %s \n", cidade1);
+    printf("Nome da Cidade: %s \n", cidade1);
 
-    printf("Populacao: %d \n", populacao1);
+    printf("Populacao: %d\n", populacao1);
 
-    printf("Area da cidade: %2f \n", area1);
+    printf("Área: %.2f km²\n", area1);
 
-    printf("PIB: %2f \n",pib1);
+    printf("PIB: %.2f bilhões de reais\n",pib1);
 
-    printf("Pontos Turisticos: %d \n",pontosturistico1);
+    printf("Número de Pontos Turísticos: %d \n",pontosturistico1);
+
+    printf("Densidade Populacional: %.2f hab/km²\n", populacao1/area1);
+    printf("PIB per Capita: %.2f reais\n", pib1/populacao1);
     
     printf("----------------------------------------------\n");
 
-    printf("CARTA 02\n\n");
+    printf("CARTA 02:\n");
     
     printf("Estado: %c \n", estado2);
 
-    printf("Codigo da Carta: %s \n", codigo2);
+    printf("Codigo: %s \n", codigo2);
 
-    printf("Nome da cidade: %s \n", cidade2);
+    printf("Nome da Cidade: %s \n", cidade2);
 
-    printf("Populacao: %d \n", populacao2);
+    printf("Populacao: %d\n", populacao2);
 
-    printf("Area da cidade: %2f \n", area2);
+    printf("Área: %.2f km²\n", area2);
 
-    printf("PIB: %2f \n",pib2);
+    printf("PIB: %.2f bilhões de reais\n",pib2);
 
-    printf("Pontos Turisticos: %d \n",pontosturistico2);
+    printf("Número de Pontos Turísticos: %d \n",pontosturistico2);
+
+    printf("Densidade Populacional: %.2f hab/km²\n", populacao2/area2);
+    printf("PIB per Capita: %.2f reais\n", pib2/populacao2);
     
     printf("----------------------------------------------\n");
     printf("COMPARAÇÃO DAS CARTAS \n");
@@ -128,6 +131,18 @@ int main() {
         printf("Pontos turísticos: Carta 1 venceu!\n");
     } else {
         printf("Pontos turísticos: Carta 2 venceu!\n");
+}
+    if (densidade1 > densidade2)
+{
+    printf("Densidade populacional: Carta 1 venceu!\n");
+} else {
+    printf("Densidade populacional: Carta 2 venceu!\n");
+}
+if (pib2 > populacao2)
+{
+    printf("PIB Percapta: Carta 1 venceu!\n");
+} else {
+    printf("PIB Percapta: Carta 2 venceu!\n");
 }
 printf("----------------------------------------------");
 }
